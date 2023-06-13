201d5c220369d9338591fb934d35c6d30124cc29
ronaldo <ronaldo.zica@sydle.com>
2023-06-13 09:29:25 -0300

    feat: contatos e loja

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,25 +1,32 @@
-import React from "react";
 import Header from "../Header/Header";
 import "./Contacts.css";
 
 function Contacts() {
-	return (
-		<div>
-			<div>
-				<Header />
-			</div>
+    return (
+        <div>
+            <div>
+                <Header />
+            </div>
 
-			<div
-				style={{
-					display: "grid",
-					placeContent: "center",
-					color: "black",
-				}}
-			>
-				<>Contatos</>
-			</div>
-		</div>
-	);
+            <div
+                style={{
+                    display: "grid",
+                    placeContent: "center",
+                    color: "black",
+                }}
+            >
+                <div>
+                    <h1>Entre em contato</h1>
+                    <div className="contact-info">
+                        <p>WhatsApp: <a href="https://wa.me/5531998648448">(31) 99864-8448</a></p>
+                        <p>Instagram: <a href="https://www.instagram.com/artesdajackeline">@artesdajackeline</a></p>
+                        <p>Email: <a href="mailto:artesdajackeline@gmail.com">artesdajackeline@gmail.com</a></p>
+                        <p>Endereço: Rua Iraci Carneiro 130, Caiçara</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Contacts;
--- src/Shop/Shop.tsx
+++ src/Shop/Shop.tsx
@@ -1,25 +1,62 @@
-import React from "react";
 import Header from "../Header/Header";
 import "./Shop.css";
+import { useEffect, useState } from "react";
+import Product, { ProductType } from "../Product/Product";
+import LoadingProduct from "../LoadingProduct/LoadingProduct";
 
 function Shop() {
-	return (
-		<div>
-			<div>
-				<Header />
-			</div>
+    const [products, setProducts] = useState<ProductType[] | null>(null);
 
-			<div
-				style={{
-					display: "grid",
-					placeContent: "center",
-					color: "black",
-				}}
-			>
-				<>Itens da loja</>
-			</div>
-		</div>
-	);
+	useEffect(() => {
+		setTimeout(() => {
+			setProducts(Array.from({ length: 16 }, (_, i) => i + 1).map((id) => (
+                {
+                    imageUrl: getLocalUrl(Math.floor(id/2))
+                }
+          )));
+		}, 100);
+	})
+
+    return (
+        <div>
+            <div>
+                <Header />
+            </div>
+
+            <div
+                style={{
+                    display: "grid",
+                    placeContent: "center",
+                    color: "black",
+                }}
+            >
+                <div>
+                    <div className="ProductList">
+                        {!products || !products.length
+                            ? Array.from({ length: 8 }, (_, i) => i + 1).map((id) => (
+                                <LoadingProduct key={id} />
+                            ))
+                            : products.map((product, index) => <Product {...product} key={index} />)}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+const localProducts = [
+    "https://cdn.discordapp.com/attachments/912368313508712488/1118136806756581428/produto1.png",
+    "https://cdn.discordapp.com/attachments/912368313508712488/1118136807025029221/produto2.png",
+    "https://cdn.discordapp.com/attachments/912368313508712488/1118136807251513354/produto3.png",
+    "https://cdn.discordapp.com/attachments/912368313508712488/1118136807490584588/produto4.png",
+    "https://cdn.discordapp.com/attachments/912368313508712488/1118136807750643742/produto5.png",
+    "https://cdn.discordapp.com/attachments/912368313508712488/1118136808048431104/produto6.png",
+    "https://cdn.discordapp.com/attachments/912368313508712488/1118136808392372234/produto7.png",
+    "https://cdn.discordapp.com/attachments/912368313508712488/1118136808685965312/produto8.png"
+];
+
+function getLocalUrl(id: number) {
+    return localProducts[id];
 }
 
 export default Shop;
