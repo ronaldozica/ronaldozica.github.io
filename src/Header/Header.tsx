8ace2f54397721b1a54efa9bd6ad114b2fcc53c1
ronaldo <ronaldo.zica@sydle.com>
2023-06-13 12:09:34 -0300

    feat: novos ícones e novo menu

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,6 +1,7 @@
 import './Header.css';
 import { Link } from 'react-router-dom';
 import HeaderItems from '../HeaderItems/HeaderItems';
+import { RiShoppingCartLine, RiUser3Line } from 'react-icons/ri';
 
 export default function Header() {
     return (
@@ -14,6 +15,19 @@ export default function Header() {
 
                 <div className="Home-items">
                     <HeaderItems />
+                </div>
+
+                <div className="CustomItems">
+                    <div>
+                        <Link to="/cart">
+                            <RiShoppingCartLine className="CartIcon" size={100} style={{ position: "relative", color: "black", width: "30px" }} />
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to="/signIn">
+                            <RiUser3Line className="SignInIcon" size={100} style={{ position: "relative", color: "black", width: "30px" }} />
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
