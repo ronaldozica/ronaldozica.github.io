1a5ac3279ad4b335f655f9a9bd40e2cb8d2fcf6c
ronaldo <ronaldo.zica@sydle.com>
2023-06-13 08:53:41 -0300

    feat: apresentação - itens locais

--- src/ServiceCard/ServiceCard.tsx
+++ src/ServiceCard/ServiceCard.tsx
@@ -1,19 +1,25 @@
 import './ServiceCard.css';
 
-export default function ServiceCard() {
+export interface ServiceCardType {
+    image: string,
+    title: string,
+    subtitle: string
+}
+
+export default function ServiceCard({ image, title, subtitle }: ServiceCardType) {
     return (
         <div>
             <div className="center">
                 <div className="property-card">
                     <a href="/">
-                        <div className="property-image">
+                        <div className="property-image" style={{backgroundImage: `url(${image})`}}>
                             <div className="property-image-title">
                                 {/* Optional <h5>Card Title</h5> If you want it, turn on the CSS also. */}
                             </div>
                         </div></a>
                     <div className="property-description">
-                        <h5> Card Title </h5>
-                        <p>Lorem Ipsum Dipsum hortata. Mixcall Horcho. Mixwell Chingo. More Bingo. Lorem Ipum doth be hard.</p>
+                        <h5> {title} </h5>
+                        <p> {subtitle} </p>
                     </div>
                     <a href="/">
                         <div className="property-social-icons">
